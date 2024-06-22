76765a27906600b2682dfa913648cff81c2e5c9a
Alex Kinyua <alexkinyua001alx@gmail.com>
2024-06-23 01:37:45 +0300

    set debug to true for debugging

--- mkulima_bora/settings.py
+++ mkulima_bora/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ch98*$a+vt1+swm!nx6ac8-8db8#levp9*!#=y=o#jmxr(p-5a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

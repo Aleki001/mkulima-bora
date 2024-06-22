b641c859bd9dcc7da4b39634f2fa674a0700bed9
Alex Kinyua <alexkinyua001alx@gmail.com>
2024-06-23 01:24:22 +0300

    allowed all sites

--- mkulima_bora/settings.py
+++ mkulima_bora/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ch98*$a+vt1+swm!nx6ac8-8db8#levp9*!#=y=o#jmxr(p-5a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
